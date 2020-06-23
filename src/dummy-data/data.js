b13893e2de0234a9c328642236dd98cb5f932338
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-06-23 10:29:12 +0200

    CRA + dummy data setup

---
+++ src/dummy-data/data.js
@@ -0,0 +1,49 @@
+const categories = [
+    "Agrarhandel",
+    "Baustoffhandel",
+    "Bauwesen",
+    "Einzelhandel/Raiffeisen",
+    "Energie",
+    "Futtermittel/Tierernährung",
+    "IT",
+    "Kaufmännisch/Administrativ",
+    "Landtechnik",
+    "Logistik/Transport",
+    "Personal",
+    "Pflanzenbau",
+    "Produktion",
+    "Sonstiges",
+    "Vertrieb/Marketing",
+  ];
+
+const data = [
+    {title: "",
+    company: "",
+    city: "",
+    date: "",
+    plz: "",
+    category: 1,
+    desc: "",
+}
+]
+
+exports.data = data
+
+
+/*
+0 "Agrarhandel",
+1 "Baustoffhandel",
+2 "Bauwesen",
+3 "Einzelhandel/Raiffeisen",
+4 "Energie",
+5 "Futtermittel/Tierernährung",
+6 "IT",
+7 "Kaufmännisch/Administrativ",
+8 "Landtechnik",
+9 "Logistik/Transport",
+10 "Personal",
+11 "Pflanzenbau",
+12 "Produktion",
+13 "Sonstiges",
+14 "Vertrieb/Marketing",
+*/

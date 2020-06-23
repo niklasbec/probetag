877ce16db7e37697690881dc3f9f0dd469bd4bbe
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-06-23 11:06:42 +0200

    some general layout and more dummy data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  
+      <h1 className="main-heading">Ihre Karriere bei AGRAVIS</h1>
+      <div className="job-search">
+        <div className="job-postings">
+
+        </div>
+        <div className="filter-pannel">
+          <input className="text-search-field" placeholder="Jobangebote durchsuchen" />
+        </div>
+      </div>
     </div>
   );
 }
